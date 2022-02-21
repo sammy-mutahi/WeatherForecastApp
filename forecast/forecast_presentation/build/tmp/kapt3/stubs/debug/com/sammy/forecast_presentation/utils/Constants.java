f680e54a663a59d73e033b971723c1d3bb9fca13
sammy-mutahi <sammymutahi21@gmail.com>
2022-02-21 20:44:19 +0300

    fix fetching user location bug

---
+++ forecast/forecast_presentation/build/tmp/kapt3/stubs/debug/com/sammy/forecast_presentation/utils/Constants.java
@@ -0,0 +1,14 @@
+package com.sammy.forecast_presentation.utils;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 6, 0}, k = 1, d1 = {"\u0000\u0012\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0002\n\u0002\u0010\b\n\u0000\b\u00c6\u0002\u0018\u00002\u00020\u0001B\u0007\b\u0002\u00a2\u0006\u0002\u0010\u0002R\u000e\u0010\u0003\u001a\u00020\u0004X\u0086T\u00a2\u0006\u0002\n\u0000\u00a8\u0006\u0005"}, d2 = {"Lcom/sammy/forecast_presentation/utils/Constants;", "", "()V", "LOCATION_PERMISSION_REQUEST_CODE", "", "forecast_presentation_debug"})
+public final class Constants {
+    @org.jetbrains.annotations.NotNull()
+    public static final com.sammy.forecast_presentation.utils.Constants INSTANCE = null;
+    public static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    
+    private Constants() {
+        super();
+    }
+}
